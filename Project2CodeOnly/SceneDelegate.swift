b969c23028996d18aa6040926f33d3be414e32bc
PatrickR <patrick@ruzman.net>
2022-05-25 17:00:39 +0200

    Added images for buttons background. Changed sceneDelegate render, from implicit unwrapping to optional. Added viewController to navController. Rendered first button with test action of print.

--- Project2CodeOnly/SceneDelegate.swift
+++ Project2CodeOnly/SceneDelegate.swift
@@ -16,14 +16,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        let windowScene: UIWindowScene = scene as! UIWindowScene
-        let navControler = navController()
+        guard let windowScene: UIWindowScene = (scene as? UIWindowScene) else {return}
+        let navController = navController()
+        let viewController = ViewController()
         
-        self.window = UIWindow(windowScene: windowScene)
+        navController.viewControllers = [viewController]
         
-        self.window!.rootViewController = navControler
-        
-        self.window!.makeKeyAndVisible()
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
 
         
         
--- Project2CodeOnly/navController.swift
+++ Project2CodeOnly/navController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class navController: UINavigationController {
     
+    let button1 = UIButton(type: .system)
+    let button2 = UIButton(type: .system)
+    let button3 = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        button1.frame = CGRect(x: 100, y: 100, width: 200, height: 100)
+        button1.setTitle("Button", for: .normal)
+        button1.backgroundColor = .lightGray
         
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        
+        button1.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        view.addSubview(button1)
+        
+    }
+    
+    @objc func buttonAction(){
+        print("Tap test")
     }
     
 }

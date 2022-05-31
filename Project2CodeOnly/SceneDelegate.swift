c751272c35b6764e628c418526b471bc6024bbab
PatrickR <patrick@ruzman.net>
2022-05-31 14:05:50 +0200

    navController & function length
    *removes navController as is unused
    *reduces function lengths to <120 chars

--- Project2CodeOnly/SceneDelegate.swift
+++ Project2CodeOnly/SceneDelegate.swift
@@ -11,10 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene: UIWindowScene = (scene as? UIWindowScene) else {return}
-        let navController = navController()
+        let navController = UINavigationController()
         let viewController = ViewController()
 
         navController.viewControllers = [viewController]

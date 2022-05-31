2287e9f0ce6003edce47b29b5768b57d84fbfebf
PatrickR <patrick@ruzman.net>
2022-05-31 19:49:11 +0200

    Adds basic tableView
    *creates countries array
    *adds tableView constraints
    *adds tableView cells

--- project3CodeOnly/ViewController.swift
+++ project3CodeOnly/ViewController.swift
@@ -9,7 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var countries = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        title = "Countries"
+        let tableView = UITableView()
+
+        countries += [
+            "estonia",
+            "france",
+            "germany",
+            "ireland",
+            "italy",
+            "monaco",
+            "nigeria",
+            "poland",
+            "russia",
+            "spain",
+            "uk",
+            "us"]
+
+        configTableView()
+        view.backgroundColor = .white
+
+        func configTableView() {
+            view.addSubview(tableView)
+            setTableViewDelegates()
+            tableView.rowHeight = 50
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "FlagCell")
+
+            tableView.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                tableView.topAnchor.constraint(equalTo: view.topAnchor),
+                tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            ])
+        }
+
+        func setTableViewDelegates() {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let tableCell = tableView.dequeueReusableCell(withIdentifier: "FlagCell") else {
+            fatalError("Wrong cell type")
+        }
+        tableCell.textLabel?.text = countries[indexPath.row]
+        return tableCell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
     }
 }

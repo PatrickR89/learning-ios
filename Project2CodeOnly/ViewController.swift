a2ef1eb641d66b3622e4003e9aec2eeb2d497e47
PatrickR <patrick@ruzman.net>
2022-06-01 10:52:09 +0200

    Adds single button customisation method
    *reduces 3 customisation functions to one
    *adds switch case for individual button params

--- Project2CodeOnly/ViewController.swift
+++ Project2CodeOnly/ViewController.swift
@@ -22,11 +22,14 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
 
         view.addSubview(button1)
-        modifyButton1()
+        customizeButton(button1)
+
         view.addSubview(button2)
-        modifyButton2()
+        customizeButton(button2)
+
         view.addSubview(button3)
-        modifyButton3()
+        customizeButton(button3)
+
 
         countries += ["estonia",
                       "france",
@@ -44,57 +47,37 @@ class ViewController: UIViewController {
     }
 }
 
-// MARK: Buttons 1 - 3 implementation
+// MARK: Buttons implementation
 
 extension ViewController {
-    func modifyButton1() {
-        button1.setTitle("", for: .normal)
-        button1.layer.borderWidth = 1
-        button1.layer.borderColor = UIColor.lightGray.cgColor
-        button1.tag = 0
-        button1.translatesAutoresizingMaskIntoConstraints = false
+
+    func customizeButton(_ button: UIButton) {
+        button.setTitle("", for: .normal)
+        button.backgroundColor = .white
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.tag = 2
+        button.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            button1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button1.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            button1.widthAnchor.constraint(equalToConstant: 200),
-            button1.heightAnchor.constraint(equalToConstant: 100)
+            button.widthAnchor.constraint(equalToConstant: 200),
+            button.heightAnchor.constraint(equalToConstant: 100),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
 
-        button1.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-    }
-
-    func modifyButton2() {
-        button2.setTitle("", for: .normal)
-        button2.backgroundColor = .green
-        button2.layer.borderWidth = 1
-        button2.layer.borderColor = UIColor.lightGray.cgColor
-        button2.tag = 1
-        button2.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            button2.widthAnchor.constraint(equalToConstant: 200),
-            button2.heightAnchor.constraint(equalToConstant: 100),
-            button2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button2.topAnchor.constraint(equalTo: button1.bottomAnchor, constant: 40)
-        ])
-
-        button2.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-    }
-
-    func modifyButton3() {
-        button3.setTitle("", for: .normal)
-        button3.backgroundColor = .red
-        button3.layer.borderWidth = 1
-        button3.layer.borderColor = UIColor.lightGray.cgColor
-        button3.tag = 2
-        button3.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            button3.widthAnchor.constraint(equalToConstant: 200),
-            button3.heightAnchor.constraint(equalToConstant: 100),
-            button3.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button3.topAnchor.constraint(equalTo: button2.bottomAnchor, constant: 40)
-        ])
-
-        button3.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        switch button {
+        case button1:
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+            button.tag = 0
+        case button2:
+            button.topAnchor.constraint(equalTo: button1.bottomAnchor, constant: 40).isActive = true
+            button.tag = 1
+        case button3:
+            button.topAnchor.constraint(equalTo: button2.bottomAnchor, constant: 40).isActive = true
+            button.tag = 2
+        default:
+            print("Missing button id")
+        }
     }
 }
 

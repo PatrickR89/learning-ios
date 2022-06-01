ab90ecfa9679b1726b575fd1b146e43a15330f77
PatrickR <patrick@ruzman.net>
2022-06-01 19:24:39 +0200

    Initial commit w/ app
    *removes IB and storyboard
    *implements programmatic UI
    *blue background for test

---
+++ project4codeOnly/project4codeOnly/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  project4codeOnly
+//
+//  Created by Patrick on 01.06.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+

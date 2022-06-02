076293bf0506565c1700b93bbcf274f0494fe366
PatrickR <patrick@ruzman.net>
2022-06-02 08:44:57 +0200

    Add navigation button for action sheet
    *adds two urls
    *adds cancel button
    *adds functionality

--- project4codeOnly/project4codeOnly/ViewController.swift
+++ project4codeOnly/project4codeOnly/ViewController.swift
@@ -21,11 +21,35 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openLinks))
+
         guard let url = URL(string: "https://www.hackingwithswift.com") else {
             print("Website not available")
             return
         }
         webView?.load(URLRequest(url: url))
         webView?.allowsBackForwardNavigationGestures = true
+    }
+
+    @objc func openLinks() {
+        let actionController = UIAlertController(title: "Open page", message: nil, preferredStyle: .actionSheet)
+        actionController.addAction(UIAlertAction(title: "www.apple.com", style: .default, handler: openPage))
+        actionController.addAction(UIAlertAction(title: "www.hackingwithswift.com", style: .default, handler: openPage))
+        actionController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        actionController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(actionController, animated: true)
+    }
+
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else {
+            print("Website not found")
+            return
+        }
+
+        guard let url = URL(string: "https://" + actionTitle) else {
+            return
+        }
+
+        webView?.load(URLRequest(url: url))
     }
 }

2955ce66684b6e9ccbde15724ff30dec0a140eee
PatrickR <patrick@ruzman.net>
2022-06-03 10:34:57 +0200

    Implements tableView with cells
    * class returned to UIViewController
    * extension edited accordingly
    * tableView configures to ViewController

--- project5codeOnly/project5codeOnly/ViewController.swift
+++ project5codeOnly/project5codeOnly/ViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController {
 
     var allWords = [String]()
     var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tableView = UITableView()
+        configTableView()
 
         if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordURL) {
@@ -28,11 +30,43 @@ class ViewController: UITableViewController {
         startGame()
 
         view.backgroundColor = .gray
-    }
 
     func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
+    }
+
+    func configTableView() {
+        view.addSubview(tableView)
+        configTableViewDelegate()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Word")
+    }
+    func configTableViewDelegate() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Word") else {
+            fatalError("No cell")
+        }
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
     }
 }

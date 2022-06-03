ca3df1bc8286d0aa5806e95bb999d6a7ca908264
PatrickR <patrick@ruzman.net>
2022-06-03 09:36:00 +0200

    Adds rendering in SceneDelegate
    *adds backgroundColor for testing

--- project5codeOnly/project5codeOnly/SceneDelegate.swift
+++ project5codeOnly/project5codeOnly/SceneDelegate.swift
@@ -15,7 +15,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions) {
+            guard let windowScene: UIWindowScene = (scene as? UIWindowScene) else {return}
+            let navController = UINavigationController()
+            let viewController = ViewController()
 
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+            navController.viewControllers = [viewController]
+
+            window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+            window?.windowScene = windowScene
+            window?.rootViewController = navController
+            window?.makeKeyAndVisible()
+        }
 }

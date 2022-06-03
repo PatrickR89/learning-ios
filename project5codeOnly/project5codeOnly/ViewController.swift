4fd6e9495843cbe973b626950b73b3370f291a0c
PatrickR <patrick@ruzman.net>
2022-06-03 13:49:05 +0200

    Add submitAction method to alertController

--- project5codeOnly/project5codeOnly/ViewController.swift
+++ project5codeOnly/project5codeOnly/ViewController.swift
@@ -82,6 +82,16 @@ extension ViewController {
     @objc func promptForAnswer() {
         let alertController = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         alertController.addTextField()
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak alertController] _ in
+            guard let answer = alertController?.textFields?[0].text else {return}
+            self?.submit(answer)
+        }
+        alertController.addAction(submitAction)
         present(alertController, animated: true)
+    }
+
+    func submit(_ answer: String) {
+
     }
 }

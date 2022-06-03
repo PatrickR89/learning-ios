d436d4a5629d144272680da8ae57ef24e4b43df4
PatrickR <patrick@ruzman.net>
2022-06-03 09:47:55 +0200

    Adds loading word functionality
    * adds method to load word from start.txt
    * adds method to place random word as title

--- project5codeOnly/project5codeOnly/ViewController.swift
+++ project5codeOnly/project5codeOnly/ViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var allWords = [String]()
+    var usedWords = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+
+        if allWords.isEmpty {
+            allWords = ["Words not loaded"]
+        }
+
+        startGame()
+
+        view.backgroundColor = .gray
+    }
+
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
 }

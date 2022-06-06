3a1dcb6c849781800370ab4788c5f2c5a780d134
PatrickR <patrick@ruzman.net>
2022-06-06 09:19:54 +0200

    Initial commit for challenge1-d32
    *initialize app
    *remove IB, storyboard
    *set up render in scene delegate
    *remove dead code & whitespace
    *red background for testing

---
+++ challenge1-d32/challenge1-d32/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  challenge1-d32
+//
+//  Created by Patrick on 06.06.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}

ca0930e397d9003b8378b99de9ed8bdcf92c81a7
PatrickR <patrick@ruzman.net>
2022-06-06 14:16:57 +0200

    Initial commit, create app

---
+++ project7codeOnly/project7codeOnly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project7codeOnly
+//
+//  Created by Patrick on 06.06.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

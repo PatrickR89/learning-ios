c3dede766072e8785e02ad1c87f4bc6198966032
PatrickR <patrick@ruzman.net>
2022-06-08 12:37:51 +0200

    Add scoreLabel setup method

--- project8codeOnly/project8codeOnly/ViewController.swift
+++ project8codeOnly/project8codeOnly/ViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cluesLabel: UILabel?
+    var answersLabel: UILabel?
+    var currentAnswer: UITextField?
+    var scoreLabel: UILabel?
+    var letterButtons = [UIButton]()
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        setScoreLabel()
+
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
-        view.backgroundColor = .systemBlue
+extension ViewController {
+    func setScoreLabel() {
+        scoreLabel = UILabel()
+
+        if let scoreLabel = scoreLabel {
+            scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+            scoreLabel.textAlignment = .right
+            scoreLabel.text = "Score: 0"
+            view.addSubview(scoreLabel)
+
+        }
     }
 }

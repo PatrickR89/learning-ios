fabe22ab881aec7b832305a2d00aee50b3aab595
PatrickR <patrick@ruzman.net>
2022-06-08 12:56:46 +0200

    Set layout constraints for:
    * scoreLabel
    * cluesLabel
    * answersLabel

--- project8codeOnly/project8codeOnly/ViewController.swift
+++ project8codeOnly/project8codeOnly/ViewController.swift
@@ -39,30 +39,55 @@ extension ViewController {
             scoreLabel.text = "Score: 0"
             view.addSubview(scoreLabel)
 
+            NSLayoutConstraint.activate([
+                scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+                scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+
+            ])
+
         }
     }
 
     func setCluesLabel() {
         cluesLabel = UILabel()
 
-        if let cluesLabel = cluesLabel {
+        if let cluesLabel = cluesLabel, let scoreLabel = scoreLabel {
             cluesLabel.translatesAutoresizingMaskIntoConstraints = false
             cluesLabel.font = UIFont.systemFont(ofSize: 24)
             cluesLabel.text = "CLUES"
             cluesLabel.numberOfLines = 0
             view.addSubview(cluesLabel)
+
+            NSLayoutConstraint.activate([
+                cluesLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+                cluesLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
+                cluesLabel.widthAnchor.constraint(
+                    equalTo: view.layoutMarginsGuide.widthAnchor,
+                    multiplier: 0.6,
+                    constant: -100)
+            ])
         }
     }
 
     func setAnswersLabel() {
         answersLabel = UILabel()
 
-        if let answersLabel = answersLabel {
+        if let answersLabel = answersLabel, let scoreLabel = scoreLabel, let cluesLabel = cluesLabel {
             answersLabel.translatesAutoresizingMaskIntoConstraints = false
             answersLabel.font = UIFont.systemFont(ofSize: 24)
             answersLabel.text = "ANSWERS"
             answersLabel.numberOfLines = 0
             view.addSubview(answersLabel)
+
+            NSLayoutConstraint.activate([
+                answersLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+                answersLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100),
+                answersLabel.widthAnchor.constraint(
+                    equalTo: view.layoutMarginsGuide.widthAnchor,
+                    multiplier: 0.4,
+                    constant: -100),
+                answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor)
+            ])
         }
     }
 }

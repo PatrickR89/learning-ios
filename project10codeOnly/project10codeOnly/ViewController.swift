52575736fa827e236801002001f4cec7b9937af8
PatrickR <patrick@ruzman.net>
2022-06-14 11:41:41 +0200

    Add collectionView
    * initialise UI layout
    * initialise flowLayout

--- project10codeOnly/project10codeOnly/ViewController.swift
+++ project10codeOnly/project10codeOnly/ViewController.swift
@@ -9,9 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout.init())
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .cyan
+
+        setCollectionView()
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+}
+
+extension ViewController {
+    func setCollectionView() {
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.scrollDirection = .horizontal
+        }
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Person")
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .black
+
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            collectionView.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor),
+            collectionView.heightAnchor.constraint(equalTo: view.layoutMarginsGuide.heightAnchor)
+        ])
     }
 }

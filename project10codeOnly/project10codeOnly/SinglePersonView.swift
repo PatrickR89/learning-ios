f3994684bccdcd7f8b73bafc89cd2cf2d65243e8
PatrickR <patrick@ruzman.net>
2022-06-18 10:17:55 +0200

    Add view controller for single person

---
+++ project10codeOnly/project10codeOnly/SinglePersonView.swift
@@ -0,0 +1,17 @@
+//
+//  SinglePersonView.swift
+//  project10codeOnly
+//
+//  Created by Patrick on 18.06.2022..
+//
+
+import UIKit
+
+class SinglePersonView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}

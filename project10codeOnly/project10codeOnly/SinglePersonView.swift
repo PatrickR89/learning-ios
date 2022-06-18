a2dffdf0c2b561c171b40c5661844acefcdc4f88
PatrickR <patrick@ruzman.net>
2022-06-18 10:56:09 +0200

    Add textField to SinglePersonView

--- project10codeOnly/project10codeOnly/SinglePersonView.swift
+++ project10codeOnly/project10codeOnly/SinglePersonView.swift
@@ -11,12 +11,13 @@ class SinglePersonView: UIViewController {
 
     var singlePerson = Person(name: "", image: "")
     var imageView = UIImageView()
+    var textField = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(singlePerson)
-        view.backgroundColor = .gray
+        view.backgroundColor = .black
         setupImageView()
+        setupText()
         // Do any additional setup after loading the view.
     }
 
@@ -26,8 +27,8 @@ class SinglePersonView: UIViewController {
 
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.layer.borderWidth = 2
-        imageView.layer.borderColor = UIColor(white: 0, alpha: 0.5).cgColor
-        imageView.layer.cornerRadius = 3
+        imageView.layer.borderColor = UIColor(white: 1, alpha: 0.5).cgColor
+        imageView.layer.cornerRadius = 5
 
         let path = FileManager.default.getImagePath(singlePerson.image)
         imageView.image = UIImage(contentsOfFile: path.path)
@@ -37,6 +38,25 @@ class SinglePersonView: UIViewController {
             imageView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 10),
             imageView.widthAnchor.constraint(equalToConstant: 180),
             imageView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+
+    func setupText() {
+        view.addSubview(textField)
+
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.textAlignment = .center
+        textField.font = UIFont.systemFont(ofSize: 30)
+        textField.text = singlePerson.name
+        textField.layer.borderColor = UIColor.white.cgColor
+        textField.textColor = .white
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 5
+
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField.widthAnchor.constraint(equalTo: imageView.widthAnchor)
         ])
     }
 }

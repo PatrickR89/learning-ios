7e46f76673c2fbb15827349be44c9e919a3fef97
PatrickR <patrick@ruzman.net>
2022-08-23 15:21:23 +0200

    #LI-18 [Add] FileManager middleware extension for filepaths

---
+++ challenge-d74-withMVVM/challenge-d74-withMVVM/Middleware/FileManager+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  FileManager+Extension.swift
+//  challenge-d74-withMVVM
+//
+//  Created by Patrick on 23.08.2022..
+//
+
+import UIKit
+
+extension FileManager {
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+
+    func getFilePath(_ name: String) -> URL {
+        let filePath = getDocumentsDirectory().appendingPathComponent(name)
+        return filePath
+    }
+}

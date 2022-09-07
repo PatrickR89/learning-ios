368565788316abd34e9e5266f0820942e5767016
PatrickR <patrick@ruzman.net>
2022-09-07 08:39:20 +0200

    [Add] include extension in cocoapods

---
+++ challenge-d90-withMVVM/ShareMemeImages/ShareViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ShareViewController.swift
+//  ShareMemeImages
+//
+//  Created by Patrick on 06.09.2022..
+//
+
+import UIKit
+import MobileCoreServices
+import RealmSwift
+
+
+@objc (ShareExtensionViewController)
+
+class ShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        handleShared()
+    }
+
+    private func handleShared() {
+        let attachments = (self.extensionContext?.inputItems.first as? NSExtensionItem)?.attachments ?? []
+        let contentType = kUTTypeData as String
+        for provider in attachments {
+            if provider.hasItemConformingToTypeIdentifier(contentType) {
+                provider.loadItem(forTypeIdentifier: contentType) { [unowned self] (data, error) in
+                    guard error == nil else {return}
+                    if let url = data as? URL,
+                       let imageData = try? Data(contentsOf: url) {
+
+                    } else {
+                        fatalError("Unable to save!")
+                    }
+                }
+            }
+        }
+    }
+}

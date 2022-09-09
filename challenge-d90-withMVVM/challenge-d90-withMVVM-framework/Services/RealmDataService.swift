6545efa9205a9834417660df31519ab473c57197
PatrickR <patrick@ruzman.net>
2022-09-09 07:48:24 +0200

    [Fix] framework RealmDataService filemanager issue
    - add framework self as target to framework

--- challenge-d90-withMVVM/challenge-d90-withMVVM-framework/Services/RealmDataService.swift
+++ challenge-d90-withMVVM/challenge-d90-withMVVM-framework/Services/RealmDataService.swift
@@ -15,19 +15,20 @@ class RealmDataService {
         guard let defaultRealm = Realm.Configuration.defaultConfiguration.fileURL else {
             fatalError("unable to create default realm")
         }
-        let realmURL = FileManager.default.getRealmFilePath("defaultRealm.realm")
+
+        let url = FileManager.default.getFilePath("defaultRealm.realm")
         var config: Realm.Configuration?
 
         if FileManager.default.fileExists(atPath: defaultRealm.path) {
             do {
-                _ = try FileManager.default.replaceItemAt(realmURL, withItemAt: defaultRealm)
+                _ = try FileManager.default.replaceItemAt(url, withItemAt: defaultRealm)
 
-                config = Realm.Configuration(fileURL: realmURL, schemaVersion: 1)
+                config = Realm.Configuration(fileURL: url, schemaVersion: 1)
             } catch {
                 print("Error info: \(error)")
             }
         } else {
-            config = Realm.Configuration(fileURL: realmURL, schemaVersion: 1)
+            config = Realm.Configuration(fileURL: url, schemaVersion: 1)
         }
         guard let config = config else {
             fatalError("unable to create config")
@@ -35,21 +36,5 @@ class RealmDataService {
 
         guard let realm = try? Realm(configuration: config) else {fatalError()}
         return realm
-    }
-}
-
-extension FileManager {
-    private func getDocumentsDirectory() -> URL {
-        guard let groupPath = FileManager().containerURL(
-            forSecurityApplicationGroupIdentifier: "group.com.ruzman.challenge-d90")
-        else {
-            fatalError("group directory not found")
-        }
-        return groupPath
-    }
-
-    func getRealmFilePath(_ name: String) -> URL {
-        let filePath = getDocumentsDirectory().appendingPathComponent(name)
-        return filePath
     }
 }

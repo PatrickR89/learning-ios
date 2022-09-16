f4b4dcb0bd4e78a496dd54fc100d259a3d3253f1
PatrickR <patrick@ruzman.net>
2022-09-16 11:39:13 +0200

    #LI-20 [Add] stackView to mainMenuView
    - [Add] UIStackView extension

---
+++ project-99/project-99/Middleware/UIStackView+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIStackView+Extension.swift
+//  project-99
+//
+//  Created by Patrick on 16.09.2022..
+//
+
+import UIKit
+
+extension UIStackView {
+    func arrangeView(with buttons: [UIButton]) {
+
+        for button in buttons {
+            button.backgroundColor = .systemBlue
+            self.addArrangedSubview(button)
+            NSLayoutConstraint.activate([
+                button.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+                button.heightAnchor.constraint(equalToConstant: 35),
+                button.widthAnchor.constraint(equalTo: self.widthAnchor)
+            ])
+        }
+    }
+}

b970d593d4e8c2afd5c1d3375049cf83f3d0d345
PatrickR <patrick@ruzman.net>
2022-09-19 10:54:56 +0200

    #LI-20 [Add] cells for username and password change
    - [Change] separate tableView into sections

---
+++ project-99/project-99/Views/SettingsViewCells/UsernameViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  UsernameViewCell.swift
+//  project-99
+//
+//  Created by Patrick on 19.09.2022..
+//
+
+import UIKit
+
+class UsernameViewCell: UITableViewCell {
+
+    private let titleLabel = UILabel()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+
+        use(AppTheme.self) {
+            $0.backgroundColor = $1.backgroundColor
+            $0.titleLabel.textColor = $1.textColor
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        self.setupSingleLable(with: titleLabel)
+        titleLabel.text = "Change username"
+    }
+}

6fa25bb48b5e2680703b282b9f9dd634a72f5a70
PatrickR <patrick@ruzman.net>
2022-09-28 13:04:52 +0200

    #LI-20 [Add] Setup game cards with model

---
+++ project-99/project-99/Models/Stats/GameCard.swift
@@ -0,0 +1,14 @@
+//
+//  GameCard.swift
+//  project-99
+//
+//  Created by Patrick on 28.09.2022..
+//
+
+import UIKit
+
+struct GameCard {
+    var id: Int
+    var image: String
+    var color: UIColor
+}

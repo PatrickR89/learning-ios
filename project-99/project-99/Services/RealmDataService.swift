ac84533857aa26e2be22ac5e38cfd8704e2dc00a
PatrickR <patrick@ruzman.net>
2022-10-11 08:45:03 +0200

    #LI-20 [Add] methods to update played and won games

--- project-99/project-99/Services/RealmDataService.swift
+++ project-99/project-99/Services/RealmDataService.swift
@@ -9,10 +9,34 @@ import Foundation
 import RealmSwift
 
 class RealmDataService {
-    private let shared = RealmDataService()
+    static let shared = RealmDataService()
     private var realm: Realm
+    private var userId: UUID?
 
     init() {
         self.realm = RealmDataProvider.shared.initiateRealm()
+        self.userId = UserContainer.shared.loadUser()
+    }
+
+    func updateTotalGames() {
+        guard let userId = userId,
+              let result = realm.object(ofType: UserGamesStats.self, forPrimaryKey: userId) else {return}
+
+        print(result.numberOfGames)
+
+        try? realm.write {
+            result.numberOfGames += 1
+        }
+    }
+
+    func updateGamesWon() {
+        guard let userId = userId,
+              let result = realm.object(ofType: UserGamesStats.self, forPrimaryKey: userId) else {return}
+
+        print(result.numOfGamesWon)
+
+        try? realm.write {
+            result.numOfGamesWon += 1
+        }
     }
 }

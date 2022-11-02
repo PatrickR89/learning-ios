f41b126c4d0cab65f6658172b6246c5c3f730f7d
PatrickR <patrick@ruzman.net>
2022-11-02 11:51:20 +0100

    #LI-20 [Fix] remove unnecessary MainNavigationController class, instead create instance in SceneDelegate

--- project-99/project-99/SceneDelegate.swift
+++ project-99/project-99/SceneDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    let navController = UINavigationController()
 
     func scene(
         _ scene: UIScene,
@@ -17,11 +18,71 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene: UIWindowScene = (scene as? UIWindowScene) else {return}
-        let navController = MainNavigationController()
+        setupLoginViewController()
 
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
+    }
+
+    func setupLoginViewController() {
+        let loginViewController = LoginViewController()
+        loginViewController.delegate = self
+        navController.setViewControllers([loginViewController], animated: true)
+    }
+}
+
+extension SceneDelegate: LoginViewControllerDelegate {
+    func loginViewController(
+        _ viewController: LoginViewController,
+        didLogUserInViewModel viewModel: MainMenuViewModel) {
+
+            let mainMenuViewController = MainMenuViewController(with: viewModel)
+            mainMenuViewController.delegate = self
+            navController.setViewControllers([mainMenuViewController], animated: true)
+        }
+}
+
+extension SceneDelegate: MainMenuViewControllerDelegate {
+    func mainMenuViewControllerDidOpenNewGame(_ viewController: MainMenuViewController) {
+        let viewController = NewGameViewController()
+        viewController.delegate = self
+        navController.pushViewController(viewController, animated: true)
+    }
+
+    func mainMenuViewController(
+        _ viewController: MainMenuViewController,
+        didReciveAccountDeletionFrom settingsViewController: SettingsViewController) {
+            RealmDataService.shared.deleteAccount()
+            UserContainer.shared.setUserId(nil)
+            setupLoginViewController()
+        }
+
+    func mainMenuViewControllerDidRecieveLogout(_ viewController: MainMenuViewController) {
+        UserContainer.shared.setUserId(nil)
+        setupLoginViewController()
+    }
+}
+
+extension SceneDelegate: NewGameViewControllerDelegate {
+    func newGameViewController(
+        _ viewController: NewGameViewController,
+        didStartNewGameWithViewModel viewModel: GameVCViewModel,
+        and stopwatch: Stopwatch) {
+
+            let viewController = GameViewController(with: viewModel, and: stopwatch)
+            viewController.delegate = self
+            navController.pushViewController(viewController, animated: true)
+        }
+
+    func newGameViewControllerDidRequestDismiss(_ viewController: NewGameViewController) {
+        navController.popViewController(animated: true)
+    }
+}
+
+extension SceneDelegate: GameViewControllerDelegate {
+    func gameViewControllerDidRequestDismiss(_ viewController: GameViewController) {
+        navController.popViewController(animated: true)
     }
 }
